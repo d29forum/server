895ef69035e31a0f37d49267c6f90a8f65d8b56c
Dave Snowberger <git@fartherout.org>
2018-01-26 10:56:07 -0800

    fixed typo on middleware call

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const client = new pg.Client(conString);
 
 /*********************************MIDDLEWARE*************************************/
 app.use(bodyParser.json);
-app.use(bodyParser.urlencoded({extended: true});
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 
 /*********************************OTHER SETUP************************************/
