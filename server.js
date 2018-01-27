3abe09323e68f1cd42faf9e84e733f26a7adb5d4
Dave Snowberger <git@fartherout.org>
2018-01-26 23:54:38 -0800

    added a couple test routes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 
 /*********************************CONST DECLARATIONS*****************************/
 const app = express();
-const PORT = process.env.PORT;
+const PORT = 3000;
 const conString = process.env.DATABASE_URL;
 const client = new pg.Client(conString);
 
@@ -22,12 +22,20 @@ client.connect();
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
 /*********************************POSTS******************************************/
+app.post('/api/db/users', (req,res) => {
+  console.logO('hit post route');
+  res.send('hit get route');
+});
 
 
 /*********************************GETS*******************************************/
-
+app.get('/api/db/users/:id', (req,res) => {
+  console.log('hit get route');
+  res.send('hit test route');
+});
 
 /*********************************PUTS*******************************************/
 
 
 /*********************************DELETES****************************************/
+
