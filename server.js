6a7002169d9c87a90abb37cef562f565502fbb8e
Dave Snowberger <git@fartherout.org>
2018-01-27 16:04:59 -0800

    refactored route organization

--- server.js
+++ server.js
@@ -22,33 +22,33 @@ client.connect();
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
 
 /*********************************POSTS******************************************/
-//USERS
+//USER MODEL
 app.post('/api/db/users', (req,res) => {
   client.query(`INSERT INTO users (first_name, last_name, email, username, password_hash, interests, created_on, num_comments, role, last_login, gravatar_hash) VALUES ($1,$2,$3,$4,'N/A',$5,to_timestamp(${Date.now()}/1000),0,$6,to_timestamp(${Date.now()}/1000),$7);`,
     [req.body.first_name, req.body.last_name, req.body.email, req.body.username, req.body.interests, req.body.role, req.body.gravatar_hash]);
 });
 
-//COMMENTS
+//COMMENT MODEL
 app.post('/api/db/comments', (req,res) => {
   client.query(`INSERT INTO comments (content, created_on, creator, thread_parent, subforum_parent) VALUES ($1, to_timestamp(${Date.now()}/1000), $2, $3, $4);`,
     [req.body.content, req.body.creator, req.body.thread_parent, req.body.subforum_parent]);
 });
 
 /*********************************GETS*******************************************/
-//USERS
+//USER MODEL
 app.get('/api/db/users/:username', (req,res) => {
   client.query(`SELECT * FROM users WHERE username=$1;`, [req.params.username])
   .then(result => res.send(result.rows));
 });
 
-//COMMENTS
-app.get('/api/db/comments/:id', (req,res) => {
-  client.query(`SELECT * FROM comments WHERE id=$1;`, [req.params.id])
+//THREAD MODEL
+app.get('/api/db/thread/:id', (req,res) => {
+  client.query(`SELECT * FROM comments INNER JOIN users ON comments.creator = users.id WHERE thread_parent=$1;`, [req.params.id])
   .then(result => res.send(result.rows));
 });
 
 /*********************************PUTS*******************************************/
-//USERS
+//USER MODEL
 app.put('/api/db/users/:username/login', (req,res) => {
   client.query(`UPDATE users SET last_login = to_timestamp(${Date.now()}/1000) WHERE username=$1;`,
     [req.params.username]);
@@ -59,19 +59,19 @@ app.put('/api/db/users/:username', (req,res) => {
     [req.body.first_name, req.body.last_name, req.body.email, req.body.username, req.body.interests, req.body.role, req.body.gravatar_hash, req.params.username]);
 });
 
-//COMMENTS
+//COMMENTS MODEL
 app.put('/api/db/comments/:id', (req,res) => {
   client.query(`UPDATE comments SET content=$1 WHERE id=$2;`,
     [req.body.content, req.params.id]);
 });
 
 /*********************************DELETES****************************************/
-//USERS
+//USER MODEL
 app.delete('/api/db/users/:username', (req,res) => {
   client.query(`DELETE FROM users WHERE username=$1;`, [req.params.username]);
 });
 
-//COMMENTS
+//COMMENT MODEL
 app.delete('/api/db/comments/:id', (req,res) => {
   client.query(`DELETE FROM comments WHERE id = $1`, [req.params.id]);
 });
