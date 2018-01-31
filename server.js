5dcfc36438c41365e1b16aa9e3440860fd0eea01
Brian Bixby <brianbixby0@gmail.com>
2018-01-31 13:25:28 -0800

    updated merge conflicts that were wrongly accepted

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const bodyParser = require('body-parser');
 /*********************************CONST DECLARATIONS*****************************/
 const app = express();
 const PORT = process.env.PORT || 3737;
-const conString = process.env.DATABASE_URL;
-//const conString = 'postgres://localhost:5432/d29forum';
+// const conString = process.env.DATABASE_URL;
+const conString = 'postgres://localhost:5432/d29forum';
 const client = new pg.Client(conString);
 
 /*********************************MIDDLEWARE*************************************/
@@ -28,10 +28,11 @@ app.post('/api/db/users', (req,res) => {
   client.query(`INSERT INTO users (first_name, last_name, username, password_hash, interests, created_on, num_comments, role, last_login, gravatar_hash) VALUES ('','',$1,'N/A','',to_timestamp(${Date.now()}/1000),0,'user',to_timestamp(${Date.now()}/1000),'');`,
     [req.body.username])
     .then(() => {
-      client.query(`SELECT id FROM users WHERE username=$1;`,
+      client.query(`SELECT * FROM users WHERE username=$1;`,
       [req.body.username])
       .then(result => {
         if (!result.rows.length) throw 'Username already exists';
+        // console.log(result.rows);
          res.status(200).send(result.rows);})
     })
     .catch(err => { console.log(err); res.status(500).send(err.code);});
@@ -120,22 +121,22 @@ app.get('/api/db/forum', (req, res) => {
 
 /*********************************PUTS*******************************************/
 //USER MODEL
-// app.put('/api/db/users/:username/login', (req,res) => {
-//   client.query(`UPDATE users SET last_login = to_timestamp(${Date.now()}/1000) WHERE username=$1;`,
-//     [req.params.username])
-//     .then(result => res.send(req.params.username));
-// });
-
 app.put('/api/db/users/:username', (req,res) => {
   if(req.body.email) {
     client.query(`UPDATE users SET first_name=$1, last_name=$2, email=$3, username=$4, interests=$5, gravatar_hash=$6 WHERE username=$7;`,
     [req.body.first_name, req.body.last_name, req.body.email, req.body.username, req.body.interests, req.body.gravatar_hash, req.params.username])
-    .then(() => res.send(req.body.username));
-  }
+    .then(() => {
+    let user = {username: req.body.username, gravatar_hash: req.body.gravatar_hash}; 
+    res.send(user);
+  })
+}
   else {
     client.query(`UPDATE users SET first_name=$1, last_name=$2, username=$3, interests=$4, gravatar_hash=$5 WHERE username=$6;`,
     [req.body.first_name, req.body.last_name, req.body.username, req.body.interests, req.body.gravatar_hash, req.params.username])
-    .then(() => res.send(req.body.username));
+    .then(() => {
+      let user = {username: req.body.username, gravatar_hash: req.body.gravatar_hash}; 
+      res.send(user);
+    })
   }
 });
 
